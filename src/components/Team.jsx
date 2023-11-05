fe60c2f4951c5525ea7401e449b9c3676e4f72a4
Dinithma <tharusha.dinithma@gmail.com>
2023-11-05 12:10:35 +0530

    UI Changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,26 +6,24 @@ export const Team = (props) => {
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
-          
         </div>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4 team">
-                
                   <div className="thumbnail">
                     {" "}
                     <img
-              src={d.img}
-              alt="..."
-              className="team-img"
-              style={{
-                borderRadius: "50%",
-                width: "200px", // Adjust the width and height as needed
-                height: "200px",
-                objectFit: "cover", // This ensures the image covers the circular area
-              }}
-            />
+                      src={d.img}
+                      alt="..."
+                      className="team-img"
+                      style={{
+                        borderRadius: "50%",
+                        width: "200px", // Adjust the width and height as needed
+                        height: "200px",
+                        objectFit: "cover", // This ensures the image covers the circular area
+                      }}
+                    />
                     <div className="caption">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
